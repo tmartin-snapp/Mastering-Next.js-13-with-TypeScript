f71924e353b841688372ea597a11c5dea26d7b2c
tmartin-snapp <tmartin@snapp-solutions.com>
2025-11-10 16:10:17 +0100

    Updating an Object

--- app/users/[id]/route.tsx
+++ app/users/[id]/route.tsx
@@ -11,14 +11,37 @@ interface Params {
 
 export function GET(
 	request: NextRequest, 
-	{ params }: Props) {
-		const { id } = React.use(params);
-		if (id > 10)
-			return NextResponse.json({ error: 'User not found'}, {status: 404})
-		
-		return NextResponse.json({ id: 1, name: 'Mosh'})
+	{ params }: Props) 
+{
+	const { id } = React.use(params);
+	if (id > 10)
+		return NextResponse.json({ error: 'User not found'}, {status: 404})
+	
+	return NextResponse.json({ id: 1, name: 'Mosh'})
 
-		//fetch
-		// not found --> 404
-		// else return data
-	}
+	//fetch
+	// not found --> 404
+	// else return data
+}
+
+export async function PUT (
+	request: NextRequest, 
+	{ params }: { params: { id: number}}) 
+{
+	//Validate the request body
+	const body = await request.json();
+	if (!body.name)
+	//If invalid, return 400
+		return NextResponse.json({ error: 'The name is not valid'}, {status: 400})
+	//Fetch the user with the given id
+	
+	if (params.id > 10)
+		return NextResponse.json({error: 'The user with the id value specified does not exist'}, {status: 404})
+
+	return NextResponse.json({ id: params.id, name: body.name})
+	//If doesn't exist, return 404
+
+	//Update the user
+	//Return the updated user
+}
+
