2a7bde63864c126ee884c96791cb4f9d9a3dd041
tmartin-snapp <tmartin@snapp-solutions.com>
2025-11-12 17:35:38 +0100

    Customizing the Upload Widget

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -16,6 +16,35 @@ const UploadPage = () => {
       )}
       <CldUploadWidget
         uploadPreset="playground"
+        options={{
+          sources: [
+            "local"
+          ],
+          multiple: false,
+          maxFiles: 5,
+          styles: {
+            palette: {
+              window: "#FFFFFF",
+              windowBorder: "#90A0B3",
+              tabIcon: "#0078FF",
+              menuIcons: "#5A616A",
+              textDark: "#000000",
+              textLight: "#FFFFFF",
+              link: "#0078FF",
+              action: "#FF620C",
+              inactiveTabIcon: "#0E2F5A",
+              error: "#F44235",
+              inProgress: "#0078FF",
+              complete: "#20B832",
+              sourceBg: "#E4EBF1"
+            },
+            fonts: {
+                default: {
+                    active: true
+                }
+            }
+          }
+        }}
         onUploadAdded={(result, options) => {
           console.log("Upload added:", result, options);
         }}
