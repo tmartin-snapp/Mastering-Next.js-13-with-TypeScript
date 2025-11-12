e91ce00550dbd33f77e2b95bd2ef22ed8a4c1d4c
tmartin-snapp <tmartin@snapp-solutions.com>
2025-11-12 17:23:21 +0100

    Showing Uploaded Images

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,16 +1,37 @@
-'use client';
+"use client";
 
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
   return (
-    <CldUploadWidget uploadPreset="playground">
-      {({ open }) => 
-        <button 
-          className="btn btn-primary" 
-          onClick={() => open()}>Upload</button>}
-    </CldUploadWidget>
+    <>
+      {publicId && (
+        <CldImage width="300" height="300" src={publicId} alt="meowl" />
+      )}
+      <CldUploadWidget
+        uploadPreset="playground"
+        onUploadAdded={(result, options) => {
+          console.log("Upload added:", result, options);
+        }}
+        onSuccess={(result, { widget }) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 };
 
