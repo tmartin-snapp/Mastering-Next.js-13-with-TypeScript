facfe465697b5e7a5f07b416ae14385e819eea91
tmartin-snapp <tmartin@snapp-solutions.com>
2025-11-13 16:18:22 +0100

    Accessing Sessions of the Client

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,13 +1,20 @@
+'use client';
+
 import Link from 'next/link'
 import React from 'react'
 import SignIn from './components/Sign-In'
+import { useSession } from 'next-auth/react'
 
 const NavBar = () => {
+  const {status, data: session} = useSession()
+
   return (
     <div className='flex bg-slate-200 p-3 space-x-3'>
       <Link href="/">Next.js</Link>
       <Link href="/users">Users</Link>
-      <SignIn />
+      { status === 'loading' && <p>Loading...</p> }
+      { status === 'authenticated' && <p>{session.user!.name}</p> }
+      { status === 'unauthenticated' && <SignIn />}
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
+import AuthProvider from "./auth/Provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NavBar />
-        <main>{children}</main>
+        <AuthProvider>
+          <NavBar />
+          <main>{children}</main>
+        </AuthProvider>
       </body>
     </html>
   );
